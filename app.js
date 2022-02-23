bb0371a12d1b391ba1161de6d9d201456e98928a
Alexandre Lima <alexandreatlima@gmail.com>
2022-02-23 09:50:50 -0300

    coutent router structure

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const app = express();
 
 app.use(express.json());
 
+const contentRouter = require("./routes/content.routes");
+app.use("/content", contentRouter);
+
 app.listen(Number(process.env.PORT), () => {
   console.log(`Server up and running at port ${process.env.PORT}`);
 });
