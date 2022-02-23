3d5ca76bcbfd976f9abcf3e622ee1e2bfbb8e84f
Alexandre Lima <alexandreatlima@gmail.com>
2022-02-23 11:33:09 -0300

    post content done

--- models/Content.model.js
+++ models/Content.model.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const contentSchema = new Schema({
-  contentName: { type: String, maxlength: 24, required: true, trim: true },
+  contentName: { type: String, maxlength: 64, required: true, trim: true },
   img: { type: String },
   contentBody: { type: String, required: true },
 });
+
+module.exports = mongoose.model("Content", contentSchema);
--- routes/content.routes.js
+++ routes/content.routes.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
+const ContentModel = require("../models/Content.model");
+
 router.post("/create-content", async (req, res) => {
   try {
+    const createdContent = await ContentModel.create(req.body);
+
+    return res.status(201).json(createdContent);
   } catch (err) {
     return res.status(500).json(err);
   }
