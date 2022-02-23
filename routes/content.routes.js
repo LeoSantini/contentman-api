d52ff4cd1cd9d5a4264aa39109ec58d747263666
Alexandre Lima <alexandreatlima@gmail.com>
2022-02-23 11:08:31 -0300

    create schema

--- routes/content.routes.js
+++ routes/content.routes.js
@@ -1,4 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+router.post("/create-content", async (req, res) => {
+  try {
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
 module.exports = router;
