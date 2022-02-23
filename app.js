a39be59e114afa90cc34ad2ddccf3c8f32151fbc
Alexandre Lima <alexandreatlima@gmail.com>
2022-02-23 11:44:48 -0300

    app and db config comments

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
+// Configurando nosso codigo para trabalhar com variaveis de ambiente utilizando a biblioteca dotenv
+// Lembre-se de criar um arquivo na raiz no projeto seguindo o modelo dentro de .exemple.env e colocar os valores nas variaveis
 require("dotenv").config();
+
+// Instaciando o express numa variavel
 const express = require("express");
-// const connect = require("./config/db.config");
-// connect();
+// Criando uma nova aplicação express
+const app = express();
+// Invocando a função para conectar com o banco de dados
+// Olhar o arquivo db.config dentro da pasta config
 require("./config/db.config")();
 
-const app = express();
-
+// Preparando nosso servidor para receber arquivos JSON
 app.use(express.json());
 
+// Enviando para o roteador content as requisições que tiverem "/content" logo no inicio
 const contentRouter = require("./routes/content.routes");
 app.use("/content", contentRouter);
 
+// Subindo o nosso servidor express
 app.listen(Number(process.env.PORT), () => {
   console.log(`Server up and running at port ${process.env.PORT}`);
 });
--- config/db.config.js
+++ config/db.config.js
@@ -1,12 +1,21 @@
+// Arquivo onde criamos a função de conexão com o banco de dados
+// Instanciado o ODM mongoose numa variavel
+// Iremos nos conectar ao MongoDB utilizando esse ODM
 const mongoose = require("mongoose");
 
+// Criando uma função assincrona pois a conexão com o banco retorna uma Promisse
 async function connect() {
   try {
+    // Utilizando o metodo connect do mongoose para realizar a conexão com o sistema de banco de dados
+    // Passamos a URI do banco de dados como parametro, no nosso caso, passamos uma variavel de ambiente
     const connection = await mongoose.connect(process.env.MONGODB_URI);
+    // Dando um retorno no console para saber se correu tudo bem
     console.log("Connected to DB:", connection.connection.name);
   } catch (err) {
+    // Colocando o erro no console caso a Promisse não resolva
     console.error("Database connection error:", err);
   }
 }
 
+// Exportando a função de conexão para invoca-la no app.js
 module.exports = connect;
